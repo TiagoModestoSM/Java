5f5b041939c46777996d742d0e455f675f290562
TiagoModestoSM <tiago.moura@ufpi.edu.br>
2025-07-31 20:26:01 -0300

    Primitivos

--- Projetos Java/Java-Test1/src/App.java
+++ Projetos Java/Java-Test1/src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int a = 12;
+        a = 14;
+        String Tiago = "meu nome é Tiago";
+        System.out.println(Tiago);
+        // tipo nomeVariavel = valor;
+        // var nomeVariavel = valor;
+
+        /* Tipos de Número Inteiro:
+         * byte: 8 bits -128 a 127
+         * short: 16 bits -32.768 a 32.767
+         * int: 32 bits -2.147.483.648 a 2.147.483.647
+         * long: 64 bits -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+        */
+
+        /*Tipos de Número Ponto Flutuante:
+         * float: 32 bits -3.40282347E+38 a 3.40282347E+38
+         * double: 64 bits -1.7976931348623157E+308 a 1.7976931348623157E+308
+         */
+
+         /*Tipos de String:
+          * String: sequência de caracteres "Palavra ou frase" - > aspas duplas
+            * char: caractere único 'A' - > aspas simples
+
+            boolean: verdadeiro ou falso true/false
+          */
     }
-}
+} 
