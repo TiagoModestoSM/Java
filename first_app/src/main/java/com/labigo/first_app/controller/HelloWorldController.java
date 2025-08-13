d2dfe3fe1e6ec9e13f4a02543f367a437597ee1d
TiagoModestoSM <tiago.moura@ufpi.edu.br>
2025-08-12 23:18:29 -0300

    RequestParam

--- first_app/src/main/java/com/labigo/first_app/controller/HelloWorldController.java
+++ first_app/src/main/java/com/labigo/first_app/controller/HelloWorldController.java
@@ -7,9 +7,11 @@ package com.labigo.first_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.labigo.first_app.domain.User;
@@ -40,8 +42,8 @@ public class HelloWorldController {
         return helloWorldService.helloWorld("Tiago");
     }
 
-    @PostMapping
-    public String helloWorldPost(@RequestBody User body) {
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id,@RequestParam(value = "filter", defaultValue = "nenhum") String filter, @RequestBody User body) {
         return "hello World!" + body.getName();
     }
 }
